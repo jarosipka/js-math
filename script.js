73a524fd9e6fc6e1b249c54164129fecf1a676e1
Jaro Šípka <97304062+jarosipka@users.noreply.github.com>
2022-12-07 13:14:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function mathOperations(x, y) {
+  console.log("Zadané parametre: " + x + ", " + y);
+  console.log("Vykonávam matematické operácie  (+ - * /)");
+  console.log(x + "+" + y + "=" + (x + y));
+  console.log(x + "-" + y + "=" + (x - y));
+  console.log(x + "*" + y + "=" + x * y);
+  console.log(x + "/" + y + "=" + x / y);
+}
+
+mathOperations(0, 0);
+mathOperations(4, 2);
